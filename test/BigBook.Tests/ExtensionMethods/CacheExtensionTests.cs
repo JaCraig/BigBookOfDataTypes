f39ab6ceecdbbc834ee247edda926f841724bcf3
James Craig <none@none>
2017-05-31 10:11:55 -0400

    Added back caching extension methods that were missing.

---
+++ test/BigBook.Tests/ExtensionMethods/CacheExtensionTests.cs
@@ -0,0 +1,17 @@
+﻿using BigBook.ExtensionMethods;
+using BigBook.Tests.BaseClasses;
+using Xunit;
+
+namespace BigBook.Tests.ExtensionMethods
+{
+    public class CacheExtensionTests : TestingDirectoryFixture
+    {
+        [Fact]
+        public void Cache()
+        {
+            int A = 1;
+            A.Cache("A");
+            Assert.Equal(A, "A".GetFromCache<int>());
+        }
+    }
+}
