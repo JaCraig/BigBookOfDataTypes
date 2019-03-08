ad8058090eaaad5f31b5b77ce8cc1a2805f1185a
James Craig <none@none>
2019-03-08 16:27:45 -0500

    Started profiling for various objects that need optimization.

---
+++ BigBook.Benchmarks/Tests/DynamoTests.cs
@@ -0,0 +1,109 @@
+﻿using BenchmarkDotNet.Attributes;
+using BigBook.Registration;
+using System.Dynamic;
+
+namespace BigBook.Benchmarks.Tests
+{
+    [RPlotExporter, RankColumn, MemoryDiagnoser]
+    public class DynamoTests
+    {
+        [Params(1000, 10000)]
+        public int Count;
+
+        public object[] Data { get; set; }
+
+        [Benchmark]
+        public object[] AnnonymousTest()
+        {
+            Data = new object[Count];
+            for (int x = 0; x < Count; ++x)
+            {
+                Data[x] = new { A = "Test data goes here" };
+            }
+            return Data;
+        }
+
+        [Benchmark(Baseline = true)]
+        public object[] ClassTest()
+        {
+            Data = new TestClass[Count];
+            for (int x = 0; x < Count; ++x)
+            {
+                Data[x] = new TestClass { A = "Test data goes here" };
+            }
+            return Data;
+        }
+
+        [Benchmark]
+        public object[] DynamoAnonymousConversionTest()
+        {
+            Data = new Dynamo[Count];
+            for (int x = 0; x < Count; ++x)
+            {
+                Data[x] = new Dynamo(new { A = "Test data goes here" });
+            }
+            return Data;
+        }
+
+        [Benchmark]
+        public object[] DynamoConversionTest()
+        {
+            Data = new Dynamo[Count];
+            for (int x = 0; x < Count; ++x)
+            {
+                Data[x] = new Dynamo(new TestClass { A = "Test data goes here" });
+            }
+            return Data;
+        }
+
+        [Benchmark]
+        public object[] DynamoExpandoConversionTest()
+        {
+            Data = new Dynamo[Count];
+            for (int x = 0; x < Count; ++x)
+            {
+                dynamic Temp = new ExpandoObject();
+                Temp.A = "Test data goes here";
+                Data[x] = new Dynamo(Temp);
+            }
+            return Data;
+        }
+
+        [Benchmark]
+        public object[] DynamoTest()
+        {
+            Data = new Dynamo[Count];
+            for (int x = 0; x < Count; ++x)
+            {
+                dynamic Temp = new Dynamo();
+                Temp.A = "Test data goes here";
+                Data[x] = Temp;
+            }
+            return Data;
+        }
+
+        [Benchmark]
+        public object[] ExpandoTest()
+        {
+            Data = new ExpandoObject[Count];
+            for (int x = 0; x < Count; ++x)
+            {
+                dynamic Temp = new ExpandoObject();
+                Temp.A = "Test data goes here";
+                Data[x] = Temp;
+            }
+            return Data;
+        }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Canister.Builder.CreateContainer(null).RegisterBigBookOfDataTypes().Build();
+        }
+
+        private class TestClass
+        {
+            public string A { get; set; }
+        }
+    }
+}
