71e8b2d4f6f77959e44accc13b59253c0e561a65
James Craig <none@none>
2019-04-16 13:39:07 -0400

    Package updates.

---
+++ BigBook.Benchmarks/Tests/GetTypeInfoTests.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using BigBook.Reflection;
+
+namespace BigBook.Benchmarks.Tests
+{
+    public class GetTypeInfoTests
+    {
+        [Benchmark]
+        public void CachedTypeOf()
+        {
+            var TypeInfo = TypeCacheFor<GetTypeInfoTests>.Type;
+        }
+
+        [Benchmark(Baseline = true)]
+        public void TypeOf()
+        {
+            var TypeInfo = typeof(GetTypeInfoTests);
+        }
+    }
+}
