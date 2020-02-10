0136857a6ec79075bd30a4577f41f7d00b7a97ca
James Craig <craig.jamesa@gmail.com>
2020-02-09 19:22:02 -0500

    Added comparison items to Canister so they can be resolved and reused.

---
+++ src/BigBook/Comparison/Module/ComparisonModule.cs
@@ -0,0 +1,30 @@
+﻿using Canister.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BigBook.Comparison.Module
+{
+    /// <summary>
+    /// Comparison module
+    /// </summary>
+    /// <seealso cref="IModule"/>
+    public class ComparisonModule : IModule
+    {
+        /// <summary>
+        /// Order to run it in
+        /// </summary>
+        public int Order { get; } = 1;
+
+        /// <summary>
+        /// Loads the module
+        /// </summary>
+        /// <param name="bootstrapper">Bootstrapper to register with</param>
+        public void Load(IBootstrapper bootstrapper)
+        {
+            if (bootstrapper == null)
+                return;
+
+            bootstrapper.Register(typeof(GenericComparer<>), ServiceLifetime.Singleton);
+            bootstrapper.Register(typeof(GenericEqualityComparer<>), ServiceLifetime.Singleton);
+        }
+    }
+}
