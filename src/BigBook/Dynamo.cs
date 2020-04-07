51887e658f649ba08166e006b368f085676cdf8b
James Craig <craig.jamesa@gmail.com>
2020-04-06 22:19:44 -0400

    Slight change to reduce memory usage with Dynamo.

--- src/BigBook/Dynamo.cs
+++ src/BigBook/Dynamo.cs
@@ -184,6 +184,42 @@ namespace BigBook
         }
 
         /// <summary>
+        /// The uninitialized object
+        /// </summary>
+        internal static readonly object UninitializedObject = new object();
+
+        /// <summary>
+        /// The lock object
+        /// </summary>
+        internal readonly object LockObject;
+
+        /// <summary>
+        /// The empty hash code
+        /// </summary>
+        private const int EmptyHashCode = 6551;
+
+        /// <summary>
+        /// Gets the type information.
+        /// </summary>
+        /// <value>The type information.</value>
+        private static DynamoTypes? TypeInfo = new DynamoTypes();
+
+        /// <summary>
+        /// The get value end_
+        /// </summary>
+        private Action<Dynamo, string, EventArgs.OnEndEventArgs>? getValueEnd_;
+
+        /// <summary>
+        /// The get value start_
+        /// </summary>
+        private Action<Dynamo, EventArgs.OnStartEventArgs>? getValueStart_;
+
+        /// <summary>
+        /// The property changed_
+        /// </summary>
+        private PropertyChangedEventHandler? propertyChanged_;
+
+        /// <summary>
         /// Gets the change log.
         /// </summary>
         /// <value>The change log.</value>
@@ -201,8 +237,8 @@ namespace BigBook
         internal DynamoClass Definition => Data.Definition;
 
         /// <summary>
-        /// Gets a value indicating whether the <see
-        /// cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+        /// Gets a value indicating whether the
+        /// <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
         /// </summary>
         public bool IsReadOnly { get; }
 
@@ -271,42 +307,6 @@ namespace BigBook
         /// </summary>
         /// <value>The hash code.</value>
         private int HashCode { get; set; }
-
-        /// <summary>
-        /// The uninitialized object
-        /// </summary>
-        internal static readonly object UninitializedObject = new object();
-
-        /// <summary>
-        /// The lock object
-        /// </summary>
-        internal readonly object LockObject;
-
-        /// <summary>
-        /// The empty hash code
-        /// </summary>
-        private const int EmptyHashCode = 6551;
-
-        /// <summary>
-        /// Gets the type information.
-        /// </summary>
-        /// <value>The type information.</value>
-        private static DynamoTypes? TypeInfo = new DynamoTypes();
-
-        /// <summary>
-        /// The get value end_
-        /// </summary>
-        private Action<Dynamo, string, EventArgs.OnEndEventArgs>? getValueEnd_;
-
-        /// <summary>
-        /// The get value start_
-        /// </summary>
-        private Action<Dynamo, EventArgs.OnStartEventArgs>? getValueStart_;
-
-        /// <summary>
-        /// The property changed_
-        /// </summary>
-        private PropertyChangedEventHandler? propertyChanged_;
 
         /// <summary>
         /// Gets or sets the <see cref="object"/> with the specified key.
@@ -425,8 +425,8 @@ namespace BigBook
         /// </summary>
         /// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
         /// <returns>
-        /// <see langword="true"/> if <paramref name="item"/> is found in the <see
-        /// cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, <see langword="false"/>.
+        /// <see langword="true"/> if <paramref name="item"/> is found in the
+        /// <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, <see langword="false"/>.
         /// </returns>
         public bool Contains(KeyValuePair<string, object?> item)
         {
@@ -490,8 +490,8 @@ namespace BigBook
         /// </summary>
         /// <param name="array">
         /// The one-dimensional <see cref="T:System.Array"/> that is the destination of the elements
-        /// copied from <see cref="T:System.Collections.Generic.ICollection`1"/>. The <see
-        /// cref="T:System.Array"/> must have zero-based indexing.
+        /// copied from <see cref="T:System.Collections.Generic.ICollection`1"/>. The
+        /// <see cref="T:System.Array"/> must have zero-based indexing.
         /// </param>
         /// <param name="arrayIndex">
         /// The zero-based index in <paramref name="array"/> at which copying begins.
@@ -605,9 +605,9 @@ namespace BigBook
         /// </summary>
         /// <param name="key">The key of the element to remove.</param>
         /// <returns>
-        /// <see langword="true"/> if the element is successfully removed; otherwise, <see
-        /// langword="false"/>. This method also returns <see langword="false"/> if <paramref
-        /// name="key"/> was not found in the original <see cref="T:System.Collections.Generic.IDictionary`2"/>.
+        /// <see langword="true"/> if the element is successfully removed; otherwise,
+        /// <see langword="false"/>. This method also returns <see langword="false"/> if
+        /// <paramref name="key"/> was not found in the original <see cref="T:System.Collections.Generic.IDictionary`2"/>.
         /// </returns>
         public bool Remove(string key)
         {
@@ -634,10 +634,10 @@ namespace BigBook
         /// </summary>
         /// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
         /// <returns>
-        /// <see langword="true"/> if <paramref name="item"/> was successfully removed from the <see
-        /// cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, <see langword="false"/>.
-        /// This method also returns <see langword="false"/> if <paramref name="item"/> is not found
-        /// in the original <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// <see langword="true"/> if <paramref name="item"/> was successfully removed from the
+        /// <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise,
+        /// <see langword="false"/>. This method also returns <see langword="false"/> if
+        /// <paramref name="item"/> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1"/>.
         /// </returns>
         public bool Remove(KeyValuePair<string, object?> item)
         {
@@ -755,8 +755,8 @@ namespace BigBook
         /// parameter. This parameter is passed uninitialized.
         /// </param>
         /// <returns>
-        /// <see langword="true"/> if the object that implements <see
-        /// cref="T:System.Collections.Generic.IDictionary`2"/> contains an element with the
+        /// <see langword="true"/> if the object that implements
+        /// <see cref="T:System.Collections.Generic.IDictionary`2"/> contains an element with the
         /// specified key; otherwise, <see langword="false"/>.
         /// </returns>
         public bool TryGetValue(string key, [MaybeNullWhen(false)] out object? value)
@@ -847,8 +847,10 @@ namespace BigBook
         /// </returns>
         protected object? RaiseGetValueEnd(string propertyName, object? value)
         {
+            if (getValueEnd_ is null)
+                return null;
             var End = new EventArgs.OnEndEventArgs { Content = value };
-            getValueEnd_?.Invoke(this, propertyName, End);
+            getValueEnd_.Invoke(this, propertyName, End);
             return End.Stop ? End.Content : null;
         }
 
@@ -862,8 +864,10 @@ namespace BigBook
         /// </returns>
         protected object? RaiseGetValueStart(string propertyName)
         {
+            if (getValueStart_ is null)
+                return null;
             var Start = new EventArgs.OnStartEventArgs { Content = propertyName };
-            getValueStart_?.Invoke(this, Start);
+            getValueStart_.Invoke(this, Start);
             return Start.Stop ? Start.Content : null;
         }
 
@@ -880,7 +884,9 @@ namespace BigBook
                 var TempChange = new Change(oldValue, newValue);
                 ChangeLog.AddOrUpdate(propertyName, TempChange, (_, __) => TempChange);
             }
-            propertyChanged_?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (propertyChanged_ is null)
+                return;
+            propertyChanged_.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         /// <summary>
